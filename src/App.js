40ee609f17fc85964d7bcfec94c9bf33222afd81
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-06 13:18:35 +0900

    wip: wip:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count] = useState(0);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
